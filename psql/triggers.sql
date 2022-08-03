148d995a25ca8a3c3152cdd106edd9977e8fca57
jmseca <joao.m.fonseca@gmail.com>
2022-08-03 16:13:29 +0100

    Before delete ad trigger base done

--- psql/triggers.sql
+++ psql/triggers.sql
@@ -2,14 +2,27 @@
 DROP TRIGGER IF EXISTS ad_sold
 
 CREATE OR REPLACE FUNCTION move_ad_to_sold () RETURNS TRIGGER AS
-    $$
+    $$ DECLARE similar_sold integer;
     BEGIN
-    similar_sold := 0
-    SELECT count(1) INTO similar_sold
-    FROM sold
-    WHERE OLD.brand=brand AND OLD.model=model AND OLD.price=price
-    AND
+        SELECT count(1) INTO similar_sold
+        FROM sold
+        WHERE OLD.brand=brand AND OLD.model=model AND OLD.price=price
+        AND OLD.year=year AND OLD.days_online=days_online
+        AND OLD.site_name=site_name AND OLD.kms=kms;
 
+        IF (similar_sold > 0) 
+        THEN
+            UPDATE sold
+            SET count = count + 1
+            WHERE OLD.brand=brand AND OLD.model=model AND OLD.price=price
+            AND OLD.year=year AND OLD.days_online=days_online
+            AND OLD.site_name=site_name AND OLD.kms=kms;
+            -- change this (days_online does not exist in ads)
+        ELSE
+            INSERT INTO sold values (OLD.);
+            -- change this (need to calculate days online from timestamp)
+        END IF;
+        RETURN OLD;
     END;
     $$ LANGUAGE plpgsql;
 
