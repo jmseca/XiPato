cb975249297495a7f5a5975f45481d21a717f81b
jmseca <82723911+jmseca@users.noreply.github.com>
2024-01-16 12:01:30 +0000

    small change

--- main/files/main.py
+++ main/files/main.py
@@ -2,7 +2,7 @@ from bots.bot import *
 from psql.XiPatoDB import *
 from scrapers.scraper import *
 
-Bot = XiPatoBot('5468290904:AAFfeT-gMee3S7UIjrm2ME41ibJIKy38oDA',5464332087)
-DB_Client = XiPatoUser('db.tecnico.ulisboa.pt','ist195749','ola')
+Bot = XiPatoBot('PLACE_BOT_ID_HERE',5464332087)
+DB_Client = XiPatoUser('db.tecnico.ulisboa.pt','ist195749','PLACE_DB_PASSWD_HERE')
 OLX_Scraper = OLXScraper()
 SV_Scraper = SVScraper()
